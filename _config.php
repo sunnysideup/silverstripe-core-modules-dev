59010b76b91872d8574e8c02b3cf0ff92d384093
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-03-08 12:28:01 +1300

    ENH: one stop shop cleanup of database

--- _config.php
+++ _config.php
@@ -2,3 +2,11 @@
 
 // You need this file if you don't have anything in the _config folder. If that folder exists
 // and is not empty then you can delete this file.
+
+use SilverStripe\Control\Director;
+
+if (! Director::isDev()) {
+    echo '
+    ~~~This should only be included in dev mode~~~
+    ';
+}
